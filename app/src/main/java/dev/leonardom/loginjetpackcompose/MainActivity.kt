d466c08ed0053a98a8d1e9b4f8be4f40610d3c53
Javier fernandez <javierfernandezdeveloper@gmail.com>
2021-12-30 04:59:08 +0100

    terminando el ejercicio

--- app/src/main/java/dev/leonardom/loginjetpackcompose/MainActivity.kt
+++ app/src/main/java/dev/leonardom/loginjetpackcompose/MainActivity.kt
@@ -1,14 +1,28 @@
 package dev.leonardom.loginjetpackcompose
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -24,10 +38,14 @@ import dev.leonardom.loginjetpackcompose.Presentation.registro.RegisterViewModel
 import dev.leonardom.loginjetpackcompose.Presentation.registro.RegistroScreen
 import dev.leonardom.loginjetpackcompose.navigation.Destination
 import dev.leonardom.loginjetpackcompose.ui.theme.LoginJetpackComposeTheme
+import kotlinx.coroutines.delay
+
 
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +58,9 @@ class MainActivity : ComponentActivity() {
                 BoxWithConstraints {
                     AnimatedNavHost(
                         navController = navController,
-                        startDestination = Destination.login.ruta
+                        startDestination = "Splash_screen"
                     ) {
+                        splashScreen(navController)
                         // creo una extension para poder llamar a mi screen
                         // si no  esto no seria posible igual que pasa en firebase realtime
                         addLogin(navController)
@@ -108,13 +127,10 @@ fun NavGraphBuilder.addLogin(
 
         // logica para mostrar y navegar
         if (viewModel.state.value.successLogin){
+
             LaunchedEffect(key1 = Unit){
                 // como tengo declarado en Destination los parametros lo puedo pasar por aqui
                 navController.navigate(Destination.home.ruta+"/$email"+"/$password"){
-                    // esto borra la pila de Activitys y nos deja en la actual que es home
-                    popUpTo(Destination.login.ruta){
-                        inclusive= true
-                    }
                 }
             }
 
@@ -209,4 +225,65 @@ fun NavGraphBuilder.addHome() {
         val password = it.arguments?.getString("password") ?: ""
         HomeScreen(email,password)
     }
+}
+
+
+
+@ExperimentalAnimationApi
+fun NavGraphBuilder.splashScreen(navController: NavController){
+
+    composable(
+        route= "Splash_screen"
+    ) {
+
+        SplashScreen(navController)
+    }
+
+
+}
+
+@Composable
+fun SplashScreen(navController: NavController){
+
+    val scale = remember { Animatable(0f) }
+
+
+    // AHORA PINTO LA PANTALLA DEL SCREEN
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+
+    ) {
+        Text(
+            text = "Splash Screen ", modifier = Modifier
+                .scale(scale.value)
+                .align(Alignment.Center), fontSize = 40.sp, color = Color.White
+        )
+    }
+
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.6f,
+            animationSpec = tween(
+                durationMillis = 1000,  // DURACION DE LA ANIMACION
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(100)
+
+        navController.navigate(Destination.login.ruta) {
+
+            //popUpTo( "Splah_screen")
+            popUpTo(Destination.login.ruta){ inclusive = true}
+            //launchSingleTop = true
+
+
+        }
+    }
+
 }
